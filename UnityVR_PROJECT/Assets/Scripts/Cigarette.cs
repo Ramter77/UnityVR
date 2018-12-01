4f74d7e3c1258dc27d66153377c7b3b3746b6de1
Ramter77 <34648210+Ramter77@users.noreply.github.com>
2018-12-01 04:12:43 +0100

    cigarette tests & skybox

---
+++ UnityVR_PROJECT/Assets/Scripts/Cigarette.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cigarette : MonoBehaviour {
+
+	public Material mLightMaterial;
+
+	private float mMin =  0.5f;
+	private float mMax = 1.25f;
+
+
+	// Use this for initialization
+	void Start () {
+		SetLightColor(mMin);
+	}
+
+	public IEnumerator Drag(Head head) {
+		//GetComponentInChildren<ParticleSystem>().Play();
+		yield return null;
+
+		/*
+		float emission = mMin;
+		float time = 0;
+
+		while (gameObject.activeSelf) {
+			//Target brightness
+			float brightness = head.TestForAudioInput() ? mMax : mMin;
+
+			//Lerp
+			time += Time.deltaTime * 0.01f;
+			emission = Mathf.Lerp(emission, brightness, time);
+
+			//Set
+			SetLightColor(emission);
+			yield return null;
+		}
+		*/
+	}
+
+	private void SetLightColor(float emission) {
+		//Figure out target color
+		Color baseColor = mLightMaterial.color;
+		Color finalColor = baseColor * Mathf.LinearToGammaSpace(emission);
+		
+		//Set in material
+		mLightMaterial.SetColor("_EmissionColor", finalColor);
+	}
+}
---
+++ UnityVR_PROJECT/Assets/Scripts/Head.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class Head : MonoBehaviour {
+
+	[Range(0, 5)]
+	public float mThreshold = 2.5f;
+	public ParticleSystem mParticleSystem;
+
+	public float mAudioStream = 3;
+	//private AudioClip mAudioStream = null;
+	private Coroutine mCurrentSmoke = null;
+
+	void Awake() {
+		//Devices
+
+		//Frequencies
+	}
+	// Use this for initialization
+	void Start () {
+		//mAudioStream = Microphone.Start("Vive", true, 3, 44100);	//44.1khz
+	}
+
+	private void OnTriggerEnter(Collider other) {
+		Debug.Log("Entered head collider: " + other);
+
+		var cigarette = other.gameObject.GetComponent<Cigarette>();
+
+		if (cigarette) {
+			mCurrentSmoke = StartCoroutine(cigarette.Drag(this));
+
+			mParticleSystem.Stop();
+		}
+	}
+
+	private void OnTriggerExit(Collider other) {
+		Debug.Log("Exited head collider: " + other);
+		if (other.gameObject.tag == "Cigarette") {
+			//if (mCurrentSmoke != null) {
+				//StopCoroutine(mCurrentSmoke);
+
+				mParticleSystem.Play();
+			//}
+		}
+	}
+
+	public bool TestForAudioInput() {
+		//Set the max amount of samples: 44100
+		//int length = mAudioStream.samples * mAudioStream.channels;
+		//float[] samples = new float[length];
+
+		//Get data
+		//mAudioStream.GetData(samples, 0);
+
+		//Average
+		//float averageSample = samples.Average() * 10000;
+
+		//If within threshold
+		//bool isInput = averageSample < mThreshold ? true: false;
+		bool isInput = mAudioStream < mThreshold ? true: false;
+
+		Debug.Log("isInput: " + isInput);
+		return isInput;
+	}
+
+/* 	private void TestAudio() {
+		while (!(Microphone.GetPosition("Vive") > 0)) {
+			//Wait
+		}
+
+		AudioSource audioSource = GetComponent<AudioSource>();
+		//audioSource.clip = mAudioStream;
+		//audioSource.Play();
+	}
+*/
+}
